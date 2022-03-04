8928615d3b69344221cf2ff55935636884a27cba
Jay Shah <jayshah5109@gmail.com>
2022-03-05 01:22:12 +0530

    initial commit

---
+++ demo/src/main/java/com/example/controller.java
@@ -0,0 +1,42 @@
+package com.example;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@ComponentScan
+@RequestMapping("/")
+public class controller {
+
+    @GetMapping("/get")
+    public @ResponseBody ResponseEntity<String> get() {
+        return new ResponseEntity<String>("GET Response", HttpStatus.OK);
+    }
+
+    @GetMapping("/get/{id}")
+    public @ResponseBody ResponseEntity<String> getById(@PathVariable String id) {
+        return new ResponseEntity<String>("GET Response"+id, HttpStatus.OK);
+    }
+
+    @PostMapping("/post")
+    public @ResponseBody ResponseEntity<String> post() {
+        return new ResponseEntity<String>("POST Response", HttpStatus.OK);
+    }
+
+    @PutMapping("/put")
+    public @ResponseBody ResponseEntity<String> put() {
+        return new ResponseEntity<String>("PUT Response", HttpStatus.OK);
+    }
+
+    @PatchMapping("/patch")
+    public @ResponseBody ResponseEntity<String> patch() {
+        return new ResponseEntity<String>("Patch Response", HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete")
+    public @ResponseBody ResponseEntity<String> delete() {
+        return new ResponseEntity<String>("Delete Response", HttpStatus.OK);
+    }
+
+}
